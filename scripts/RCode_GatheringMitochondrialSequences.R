5ec17f839e624bdf016e11f4e17887f49cb2bb19
MartinGenner <64019956+MartinGenner@users.noreply.github.com>
2022-08-11 00:19:50 +0100

    Update RCode_GatheringMitochondrialSequences.R

--- scripts/RCode_GatheringMitochondrialSequences.R
+++ scripts/RCode_GatheringMitochondrialSequences.R
@@ -6,8 +6,8 @@
 
 library(AnnotationBustR)
 
-#Gather the mitochondrial genomes for 14 species using Genbank accessions codes
+#Gather the mitochondrial genomes for 13 species using Genbank accessions codes
 
-ncbi.accessions<-c("AP012953", "AP004101", "AP004104", "AP004105", "AP004106", "AP004110", "AP012952", "AP012954", "AP017322", "AP017325", "AP017327", "AP017328", "HM106490", "HM106493")
+ncbi.accessions<-c("AP012953", "AP004101", "AP004104", "AP004105", "AP004106", "AP004110", "AP012952", "AP012954", "AP017322", "AP017325", "AP017328", "HM106490", "HM106493")
 
 my.sequences_data<-AnnotationBust(ncbi.accessions, mtDNAterms, DuplicateSpecies=TRUE)
